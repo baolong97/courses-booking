7f71f6e33e3ef0b1b3bdd7aaa9881960e1607698
Long Pham <long29031997@gmail.com>
2022-10-17 19:43:13 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: '*', credentials: true },
+    cors: { origin: '*' },
   });
-  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
